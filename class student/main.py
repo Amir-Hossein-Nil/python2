5c4cdf916db7051ac5d2e1840f25b84bf0cedb3d
Amir Hossein <130904044+Amir-Hossein-Nil@users.noreply.github.com>
2023-11-25 19:28:58 +0330

    Add files via upload

--- class student/main.py
+++ class student/main.py
@@ -1,15 +1,19 @@
 class student():
     def __int__(self, first_name, last_name, age, math_score, physics_score, python_score):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.math_score = math_score
-        self.physics_score = physics_score
-        self.python_score = python_score
+        self.first_name = input("first_name")
+        self.last_name = input("last_name")
+        self.age = input("age")
+        self.math_score = input("math_score")
+        self.physics_score = input("physics_score")
+        self.python_score = input("python_score")
 
     def check_status(self):
-        average_score = (self.math_score + self.physics_score + self.python_score) / 3
-        if average_score >= 17 :
+        average_score = (int(self.math_score) + int(self.physics_score) + int(self.python_score)) / 3
+        if average_score >= 17:
             print("accepted")
         elif average_score < 17:
             print("failed")
+
+
+student1 = student()
+student.check_status()
